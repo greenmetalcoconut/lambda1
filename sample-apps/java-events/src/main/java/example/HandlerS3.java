e3c73596e134a4382022920036a4ac2503f88eb2
ollysk <2x2eq4co@gmail.com>
2025-12-29 14:16:49 +0100

    param store add

--- sample-apps/java-events/src/main/java/example/HandlerS3.java
+++ sample-apps/java-events/src/main/java/example/HandlerS3.java
@@ -5,14 +5,24 @@ import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.lambda.runtime.events.models.s3.S3EventNotification.S3EventNotificationRecord;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ssm.SsmClient;
+import software.amazon.awssdk.services.ssm.model.GetParameterRequest;
+import software.amazon.awssdk.services.ssm.model.GetParameterResponse;
 
 // Handler value: example.Handler
 public class HandlerS3 implements RequestHandler<S3Event, String>{
-    
+
+    private final SsmClient ssmClient = SsmClient.builder().region(Region.EU_NORTH_1).build();
+
     @Override
     public String handleRequest(S3Event event, Context context)
     {
+
+        GetParameterRequest parameterRequest = GetParameterRequest.builder().name("/dev/bucket-name").build();
+        GetParameterResponse parameter = ssmClient.getParameter(parameterRequest);
         LambdaLogger logger = context.getLogger();
+        logger.log("PARMA VAL: "+ parameter.parameter().value());
         logger.log("EVENT: " + event);
         logger.log("CONTEXT: " + context);
         S3EventNotificationRecord record = event.getRecords().get(0);
